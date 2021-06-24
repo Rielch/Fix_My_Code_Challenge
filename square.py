ff5eaf876aba5b712f8c7adf3387de559fbcbc0a
Gabriel Prochnau <2288@holbertonschool.com>
2021-06-24 00:43:04 +0000

    Add fixes

--- square.py
+++ square.py
@@ -1,23 +1,28 @@
 #!/usr/bin/python3
+"""Square class"""
 
-class square():
-    
+
+class Square():
+    """Square class"""
     width = 0
     height = 0
 
-    
+
     def __init__(self, *args, **kwargs):
+        """Init method"""
         for key, value in kwargs.items():
             setattr(self, key, value)
 
     def area_of_my_square(self):
         """ Area of the square """
-        return self.width * self.width
+        return self.width * self.height
 
-    def PermiterOfMySquare(self):
+    def permiter_of_my_square(self):
+        """Perimeter of the square"""
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
+        """String representation of the square"""
         return "{}/{}".format(self.width, self.height)
 
 if __name__ == "__main__":
